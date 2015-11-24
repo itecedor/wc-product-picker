ef4d8be10de945618a700be7b9c04aceefbe215c
Ivete Tecedor <yivich@gmail.com>
2015-11-23 20:32:55 -0500

    fix pep8 issues

--- product-picker.py
+++ product-picker.py
@@ -19,13 +19,13 @@ r = wcapi.get('products')
 last_url = r.links['last']['url']
 url_object = urlparse(last_url)
 queries = parse_qs(url_object.query)
-page_count = queries['page'][0] # was a string, need to convert to int
+page_count = queries['page'][0]  # was a string, need to convert to int
 
 # pick a random page number to pull up and get those 10 products
-page = randint(1,int(page_count))
+page = randint(1, int(page_count))
 r = wcapi.get('products?page=' + str(page)).json()
 
 # choose one of the 10 products in the list of products and return its url
-product_number = randint(0,9)
+product_number = randint(0, 9)
 url = r['products'][product_number]['permalink']
 print url
